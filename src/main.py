07950c7d52b876793e048c56463d066cc4b64486
philipsteingruber <philip.steingruber@gmail.com>
2024-04-22 14:16:46 +0200

    Added resources

--- src/main.py
+++ src/main.py
@@ -1,1 +1,30 @@
+import pygame.display
+
 from settings import *
+from sys import exit
+import pytmx
+
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('PYkémon')
+
+    @staticmethod
+    def import_assets() -> dict[str: pytmx.TiledMap]:
+        return {'world': pytmx.util_pygame.load_pygame('../data/maps/world.tmx')}
+
+    def run(self) -> None:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+                pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,5 @@
 import pygame
-from pygame.math import Vector2 as vector 
-from sys import exit
+from pygame.math import Vector2 as vector
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 TILE_SIZE = 64 
