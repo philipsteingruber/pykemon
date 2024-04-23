71b668d600f6f5314182d0574582bd17395e6557
philipsteingruber <philip.steingruber@gmail.com>
2024-04-23 08:42:18 +0200

    .

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import pygame.display
 from settings import *
 from sys import exit
 import pytmx
+from typing import Tuple
+from sprites import Sprite
+from entities import Player
 
 
 class Game:
@@ -11,9 +14,18 @@ class Game:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('PYkémon')
 
+        self.all_sprites = pygame.sprite.Group()
+
+        self.tmx_maps = Game.import_maps()
+        self.setup(self.tmx_maps['world'], 'house')
+
     @staticmethod
-    def import_assets() -> dict[str: pytmx.TiledMap]:
+    def import_maps() -> dict[str: pytmx.TiledMap]:
         return {'world': pytmx.util_pygame.load_pygame('../data/maps/world.tmx')}
+
+    def setup(self, tmx_map: pytmx.TiledMap, player_start_pos: str) -> None:
+        for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
 
     def run(self) -> None:
         while True:
@@ -22,6 +34,7 @@ class Game:
                     pygame.quit()
                     exit()
 
+                self.all_sprites.draw(self.display_surface)
                 pygame.display.update()
 
 
---
+++ src/sprites.py
@@ -0,0 +1,10 @@
+import pygame.sprite
+
+from settings import *
+
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf: pygame.Surface, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(topleft=pos)
